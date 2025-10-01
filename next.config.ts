6f05824320fe5ed726f88e65efa8ebc2ee84b66d
muatta Muhariq <attajr59@gmail.com>
2025-10-01 15:55:23 +0700

    Add GitHub Actions workflow for deployment to muts.me

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: '',
+  basePath: ''
 };
+module.exports = nextConfig
 
 export default nextConfig;
